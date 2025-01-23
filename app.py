cccbfc93824e570509b081ca36f468e4d5ba0287
William Fecteau <william.fecteau.1@ulaval.ca>
2025-01-23 10:55:56 -0500

    Production deployment in dockerfile

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask, jsonify
 from vehicles import vehicles_bp
 from cranes import cranes_bp
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(vehicles_bp)
+    app.register_blueprint(cranes_bp)
 
-app.register_blueprint(vehicles_bp)
-app.register_blueprint(cranes_bp)
+    @app.route("/")
+    def root():
+        return jsonify({"message": "Welcome to the design 3 API."})
 
-@app.route("/")
-def root():
-    return jsonify({"message": "Welcome to the design 3 API."})
+    return app
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app = create_app()
+
+    app.logger.warning("Starting the app in development mode.")
+    app.run(host='0.0.0.0', debug=True)
